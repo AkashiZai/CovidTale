f4d999c14ae2321f78160a81dc06a0c0c7493c9c
AkashiZai <motherfcuker11@gmail.com>
2026-02-17 19:36:46 +0700

    Walk

--- CovidTale/CovidTale/src/main/Display.java
+++ CovidTale/CovidTale/src/main/Display.java
@@ -3,28 +3,28 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends GamePanel{
-    private String title;
+public class Display { // REMOVED "extends GamePanel"
 
-    public Display(String title){ //เอาไว้เรียกใช้ทุกอย่าง
+    public Display(String title){
         createDisplay(title);
     }
 
-    private void createDisplay(String title){ //สร้างจอภาพ + GamePanel
+    private void createDisplay(String title){
         JFrame d = new JFrame();
+
+        // This is the GamePanel that will actually be on screen
         GamePanel g = new GamePanel();
 
         d.add(g);
-        d.pack();
+        d.pack(); // Important: This tells the Window to resize to fit the GamePanel
 
-        g.setBackground(Color.black);
+        g.setBackground(Color.black); // Make background black so you can see the white player
         d.setTitle(title);
+        d.setResizable(false); // Usually good for 2D games
         d.setLocationRelativeTo(null);
         d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         d.setVisible(true);
+        g.startGameThread();
+        g.requestFocusInWindow();
     }
-
-
-
-
 }
--- CovidTale/CovidTale/src/main/GamePanel.java
+++ CovidTale/CovidTale/src/main/GamePanel.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.*;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicTreeUI;
 
 public class GamePanel extends JPanel implements Runnable{
     final int tile = 16; // 16*16
@@ -13,11 +14,21 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenColumn;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Key keyH = new Key();
+
     Thread gameThread;
+
+    //set default please default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerspeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -25,21 +36,55 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
-    public void run(){
-        while(gameThread != null){
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / 60; // 60 FPS
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+
             update();
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000; // Convert nanoseconds to milliseconds
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public void update(){
-
+    public void update() {
+        // CHANGED: Removed 'else' so you can move diagonally
+        if (keyH.upPressed == true) {
+            playerY -= playerspeed;
+        }
+        if (keyH.downPressed == true) {
+            playerY += playerspeed;
+        }
+        // FIXED: Changed '=' to '=='
+        if (keyH.leftPressed == true) {
+            playerX -= playerspeed;
+        }
+        if (keyH.rightPressed == true) {
+            playerX += playerspeed;
+        }
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setColor(Color.white);
-        g2.fillRect(100,100,tileSize,tileSize);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
         g2.dispose();
     }
 }
